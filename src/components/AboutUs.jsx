2d36e9e292591d7ddb2ea572c98ccc596e216c25
ppanwar <ppanwar@cholachariots.com>
2024-02-23 16:39:15 +0530

    new update

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import aboutUsImage from './images/about.jpeg'; // Renamed the import to follow JavaScript naming conventions
+import aboutUsImage from './images/product3.jpg'; // Renamed the import to follow JavaScript naming conventions
 
 const AboutUs = () => {
   return (
@@ -12,7 +12,11 @@ const AboutUs = () => {
       {/* Text content on the right */}
       <div className="lg:w-1/2 lg:mb-[110px] lg:pl-8 lg:flex lg:flex-col lg:justify-center">
   <h2 className="text-4xl  
-   font-bold mb-4 lg:mb-[100px] lg:text-left lg:ml-[260px] text-center">About Us</h2>
+   font-bold mb-4 lg:mb-[50px]
+    md:text-center
+    
+   lg:text-left
+    lg:ml-[260px] text-center md:mt-5">About Us</h2>
   <p className="text-center  lg:text-[17px] 
   lg:text-center font-normal text-base leading-relaxed mb-6 lg:mb-8 font-poppins">
     VITPARK leads digital transformation with advanced payment gateways, last mile connectivity solutions, mobility services, comprehensive e-business support, and expert digital marketing strategies. Our state-of-the-art IT park fosters innovation and collaboration. Join us for cutting-edge solutions and a culture of excellence in the digital age. Experience limitless possibilities with VITPARK. We are your partner in navigating the digital landscape, offering innovative solutions and fostering a community of forward-thinking pioneers. Join us for unparalleled expertise and endless opportunities.
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,11 @@ import product from '../components/images/cholaLogo2.png'
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900  lg:h-[230px] text-white py-6">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <footer className="bg-gray-900 text-white py-6">
+      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
           {/* First Column */}
-          <div>
-            <p className="text-sm">&copy; 2024 VIT Park. All rights reserved.</p>
-          </div>
-          {/* Second Column */}
-          <div>
+          <div className="col-span-2 md:col-span-1">
             <ul className="space-y-2">
               <li><div className="text-sm hover:text-gray-300">Privacy Policy</div></li>
               <li><div className="text-sm hover:text-gray-300">Terms of Service</div></li>
@@ -20,8 +16,8 @@ const Footer = () => {
               <li><div className="text-sm hover:text-gray-300">About</div></li>
             </ul>
           </div>
-          {/* Third Column */}
-          <div>
+          {/* Second Column */}
+          <div className="col-span-2 md:col-span-1">
             <ul className="space-y-2">
               <li><div className="text-sm hover:text-gray-300">FAQs</div></li>
               <li><div className="text-sm hover:text-gray-300">Blog</div></li>
@@ -29,8 +25,8 @@ const Footer = () => {
               <li><div className="text-sm hover:text-gray-300">Press</div></li>
             </ul>
           </div>
-          {/* Fourth Column */}
-          <div>
+          {/* Third Column */}
+          <div className="col-span-2 md:col-span-1">
             <ul className="space-y-2">
               <li><div className="text-sm hover:text-gray-300">Partners</div></li>
               <li><div className="text-sm hover:text-gray-300">Support</div></li>
@@ -38,28 +34,34 @@ const Footer = () => {
               <li><div className="text-sm hover:text-gray-300">Sitemap</div></li>
             </ul>
           </div>
-          <div className="col-span-1 md:w-full lg:w-[350px]">
-  <div className="bg-white p-4 md:p-6 lg:p-4 lg:h-[180px] lg:mr-9 rounded">
-    <div className="flex flex-col md:flex-row justify-between items-center md:mb-4">
-      {/* Our Partner */}
-      <div className="md:mb-0 md:mr-4">
-        <a href="https://biowms.com/" className="text-[17px] font-bold mb-2 text-gray-900 inline-block">
-          Our Partner
-        </a>
-        <img src={patner} alt="Partner Image" className="w-20 h-20 mb-2" />
-        <h1 className="text-gray-900 font-semibold">Biowms</h1>
-      </div>
-      
-      {/* Our Product */}
-      <div className="flex items-center flex-col">
-        <h4 className="text-[17px] font-bold mb-2 text-gray-900">Our Product</h4>
-        <img src={product} alt="Product Image" className="w-30 h-20 mb-2" />
-        <h1 className="text-gray-900 font-semibold">CHOLA cHARITOTS</h1>
-      </div>
-    </div>
-  </div>
-</div>
+          {/* Fourth Column */}
+          <div className="col-span-2 md:col-span-1">
+            <div className="bg-white p-4 w-[300px] h-[180px] rounded">
+              <div className="flex justify-between items-center mb-4">
+                {/* Our Partner */}
+                <div className='lg:mb-8'>
+                  <a href="https://biowms.com/">
+                    <h4 className="text-[17px] font-bold mb-2 text-gray-900">Our Partner</h4>
+                    <img src={patner} alt="Partner Image" className="w-20 h-20 mb-2" />
+                    <h1 className='text-gray-900'>Chola Chariots</h1>
+                  </a>
+                </div>
+                {/* Our Product */}
+                <div className="flex items-center lg:mb-6 flex-col">
+                  <h4 className="text-[17px] font-bold mb-2 text-gray-900">Our Product</h4>
+                  <img src={product} alt="Product Image" className="w-30 h-20 mb-2" />
+                  <h1 className='text-gray-900'>Chola Chariotsf</h1>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+      {/* All rights reserved */}
+      <div className="max-w-8xl
+      lg:mr-[840px]
+      mx-auto px-4 sm:px-6 lg:px-10 text-center mt-4">
+        <p className="text-sm">&copy; 2024 VIT Park. All rights reserved.</p>
       </div>
     </footer>
   );
